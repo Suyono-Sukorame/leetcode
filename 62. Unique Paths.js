bfacbf0ef50d3f7da99be34df4b72db1ad8d343f
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-27 15:37:25 +0800

    62. Unique Paths

---
+++ 62. Unique Paths.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    const dp = new Array(m).fill().map(() => new Array(n).fill(0));
+
+    dp[0][0] = 1;
+
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = 1;
+    }
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+};
+
+console.log(uniquePaths(3, 7));
+console.log(uniquePaths(3, 2));
