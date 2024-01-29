11053ae1169ea144fa09d5de62e4823d01494535
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-29 20:43:18 +0800

    189. Rotate Array

---
+++ Studyplan/189. Rotate Array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void}
+ */
+var rotate = function (nums, k) {
+  k = k % nums.length;
+
+  reverse(nums, 0, nums.length - 1);
+
+  reverse(nums, 0, k - 1);
+
+  reverse(nums, k, nums.length - 1);
+};
+
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} start
+ * @param {number} end
+ */
+var reverse = function (nums, start, end) {
+  while (start < end) {
+    const temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+
+    start++;
+    end--;
+  }
+};
