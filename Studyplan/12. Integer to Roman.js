a8efc6f9cccd7d039f0e3fc1ccfd5a54d5c9ad87
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-30 06:43:25 +0800

    12. Integer to Roman

---
+++ Studyplan/12. Integer to Roman.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  const romanSymbols = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+
+  let result = "";
+
+  for (const symbol of romanSymbols) {
+    while (num >= symbol.value) {
+      result += symbol.symbol;
+      num -= symbol.value;
+    }
+  }
+
+  return result;
+};
+
+console.log(intToRoman(3)); // Output: "III"
+console.log(intToRoman(58)); // Output: "LVIII"
+console.log(intToRoman(1994)); // Output: "MCMXCIV"
