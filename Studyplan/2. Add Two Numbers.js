f79ac9aba79c20abd448667b5a656955792a84a0
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-01-31 21:04:06 +0800

    2. Add Two Numbers

---
+++ Studyplan/2. Add Two Numbers.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val);
+ *     this.next = (next===undefined ? null : next);
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let dummyHead = new ListNode(0);
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 || l2 || carry) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = Math.floor(sum / 10);
+
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+  }
+
+  return dummyHead.next;
+};
+
+// Example usage:
+// Constructing linked lists for the example
+const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+console.log(addTwoNumbers(l1, l2));
+// Output: ListNode { val: 7, next: ListNode { val: 0, next: ListNode { val: 8, next: null } } }
