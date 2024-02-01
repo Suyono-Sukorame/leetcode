dd5fa398faa2494ce2ebb640349ec5959c8fdd00
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-02 06:39:31 +0800

    22. Generate Parentheses

---
+++ Studyplan/top-interview-150/22. Generate Parentheses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const result = [];
+
+  const generate = (current, open, close) => {
+    if (current.length === 2 * n) {
+      result.push(current);
+      return;
+    }
+
+    if (open < n) {
+      generate(current + "(", open + 1, close);
+    }
+
+    if (close < open) {
+      generate(current + ")", open, close + 1);
+    }
+  };
+
+  generate("", 0, 0);
+
+  return result;
+};
+
+// Example usage:
+console.log(generateParenthesis(3));
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+console.log(generateParenthesis(1));
+// Output: ["()"]
