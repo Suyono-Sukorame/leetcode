e17835651a5597f9405e4295ac78e4034b6b3f34
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-03 06:35:11 +0800

    392. Is Subsequence

---
+++ LeetCode 75/392. Is Subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let sIndex = 0;
+
+  for (let i = 0; i < t.length && sIndex < s.length; i++) {
+    if (s[sIndex] === t[i]) {
+      sIndex++;
+    }
+  }
+
+  return sIndex === s.length;
+};
