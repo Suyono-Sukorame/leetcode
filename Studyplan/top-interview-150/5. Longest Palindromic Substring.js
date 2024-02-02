3f5daa45aead5c4e80e60d0460d8bc272ec4f8de
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-02 10:59:02 +0800

    5. Longest Palindromic Substring

---
+++ Studyplan/top-interview-150/5. Longest Palindromic Substring.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    if (s.length <= 1) {
+        return s;
+    }
+
+    let start = 0;
+    let maxLength = 1;
+
+    function expandAroundCenter(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentLength = right - left + 1;
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+
+    for (let i = 0; i < s.length - 1; i++) {
+        expandAroundCenter(i, i);
+
+        expandAroundCenter(i, i + 1);
+    }
+
+    return s.substring(start, start + maxLength);
+};
+
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));
