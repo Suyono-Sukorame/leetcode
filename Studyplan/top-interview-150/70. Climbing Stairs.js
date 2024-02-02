68b96e10c0d906d004bf87471b1b0cc2422752a0
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-02 10:33:58 +0800

    70. Climbing Stairs

---
+++ Studyplan/top-interview-150/70. Climbing Stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
+
+// Test cases
+console.log(climbStairs(2)); // Output: 2
+console.log(climbStairs(3)); // Output: 3
