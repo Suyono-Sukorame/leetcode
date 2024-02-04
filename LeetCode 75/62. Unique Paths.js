ce9e891f713858e93e9dd4ca5ba55abc07e5b3e0
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-04 08:06:26 +0800

    62. Unique Paths

---
+++ LeetCode 75/62. Unique Paths.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  var dp = Array(m);
+  if (!m || !n) return 0;
+  for (var i = 0; i < m; i++) {
+    dp[i] = Array(n);
+    for (var j = 0; j < n; j++) {
+      if (j > 0 && i > 0) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      else if (j > 0 && i === 0) dp[i][j] = dp[i][j - 1];
+      else if (j === 0 && i > 0) dp[i][j] = dp[i - 1][j];
+      else dp[i][j] = 1;
+    }
+  }
+  return dp[m - 1][n - 1];
+};
