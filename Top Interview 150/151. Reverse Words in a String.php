c1b64e7a58cf640fedbe3bacd9391a6f14f058a5
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-05 18:24:40 +0800

    151. Reverse Words in a String

---
+++ Top Interview 150/151. Reverse Words in a String.php
@@ -0,0 +1,14 @@
+class Solution {
+
+/**
+ * @param String $s
+ * @return String
+ */
+function reverseWords($s) {
+    $arr = array_diff(explode(' ', trim($s)), ['']);
+    $arr = array_reverse($arr);
+
+    return implode(' ', $arr);
+}
+}
+
