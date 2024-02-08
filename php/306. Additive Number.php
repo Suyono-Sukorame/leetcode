663d3f54b6978e7efcbf4d29e3d1ba0d5b61be9d
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-09 06:56:38 +0800

    306. Additive Number

--- php/306. Additive Number.php
+++ php/306. Additive Number.php
@@ -6,27 +6,24 @@ class Solution {
  */
 function isAdditiveNumber($num) {
     $n = strlen($num);
-    for ($i = 1; $i <= intval($n / 2); $i++) {
-        if ($i > 1 && $num[0] == '0') break;
-        for ($j = 1; $i + $j < $n; $j++) {
-            if ($j > 1 && $num[$i] == '0') break;
-            $num1 = substr($num, 0, $i);
-            $num2 = substr($num, $i, $j);
-            if ($this->isValid($num1, $num2, $num, $i + $j)) return true;
+    for ($i = 1; $i <= intval($n / 2); ++$i) {
+        if ($num[0] == '0' && $i > 1) return false;
+        $x1 = substr($num, 0, $i);
+        for ($j = 1; max($j, $i) <= $n - $i - $j; ++$j) {
+            if ($num[$i] == '0' && $j > 1) break;
+            $x2 = substr($num, $i, $j);
+            if ($this->isValid($x1, $x2, $j + $i, $num)) return true;
         }
     }
     return false;
 }
 
-function isValid($num1, $num2, $num, $start) {
-    $n = strlen($num);
-    while ($start < $n) {
-        $sum = (string)((int)$num1 + (int)$num2);
-        if (!strpos($num, $sum, $start) === $start) return false;
-        $start += strlen($sum);
-        $num1 = $num2;
-        $num2 = $sum;
-    }
-    return true;
+private function isValid($x1, $x2, $start, $num) {
+    if ($start == strlen($num)) return true;
+    $x2 = bcadd($x1, $x2);
+    $x1 = bcsub($x2, $x1);
+    $sum = $x2;
+    if (strpos($num, $sum, $start) !== $start) return false;
+    return $this->isValid($x1, $x2, $start + strlen($sum), $num);
 }
 }
