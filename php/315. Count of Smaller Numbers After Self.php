eecaf9d18d8078a9c9bad4cf9d091b024f277673
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-09 21:53:03 +0800

    315. Count of Smaller Numbers After Self

--- php/315. Count of Smaller Numbers After Self.php
+++ php/315. Count of Smaller Numbers After Self.php
@@ -1,33 +1,61 @@
 class Solution {
 
+// private $sorted;
+// private $count;
+
 /**
  * @param Integer[] $nums
  * @return Integer[]
  */
-function countSmaller($nums) {
+public function countSmaller($nums) {
     $n = count($nums);
-    $result = array_fill(0, $n, 0);
-    $sorted = [];
-    for ($i = $n - 1; $i >= 0; $i--) {
-        $num = $nums[$i];
-        $index = $this->binarySearch($sorted, $num);
-        $result[$i] = $index;
-        array_splice($sorted, $index, 0, $num);
-    }
-    return $result;
+    $counts = array_fill(0, $n, 0);
+    $index_map = array_keys($nums);
+    $sorted_nums = array_fill(0, $n, 0);
+    $this->mergeSort($nums, $sorted_nums, $index_map, $counts, 0, $n - 1);
+    return $counts;
 }
 
-function binarySearch(&$sorted, $target) {
-    $left = 0;
-    $right = count($sorted) - 1;
-    while ($left <= $right) {
-        $mid = (int)($left + ($right - $left) / 2);
-        if ($sorted[$mid] < $target) {
-            $left = $mid + 1;
-        } else {
-            $right = $mid - 1;
-        }
+private function mergeSort(&$nums, &$sorted_nums, &$index_map, &$counts, $left, $right) {
+    if ($left >= $right) {
+        return;
     }
-    return $left;
+    $mid = (int)(($left + $right) / 2);
+    $this->mergeSort($nums, $sorted_nums, $index_map, $counts, $left, $mid);
+    $this->mergeSort($nums, $sorted_nums, $index_map, $counts, $mid + 1, $right);
+    $this->merge($nums, $sorted_nums, $index_map, $counts, $left, $mid, $right);
+}
+
+private function merge(&$nums, &$sorted_nums, &$index_map, &$counts, $left, $mid, $right) {
+    $i = $left;
+    $j = $mid + 1;
+    $k = $left;
+    $num_elements_right = 0;
+    while ($i <= $mid && $j <= $right) {
+        if ($nums[$index_map[$i]] <= $nums[$index_map[$j]]) {
+            $sorted_nums[$k] = $index_map[$i];
+            $counts[$index_map[$i]] += $num_elements_right;
+            $i++;
+        } else {
+            $sorted_nums[$k] = $index_map[$j];
+            $num_elements_right++;
+            $j++;
+        }
+        $k++;
+    }
+    while ($i <= $mid) {
+        $sorted_nums[$k] = $index_map[$i];
+        $counts[$index_map[$i]] += $num_elements_right;
+        $i++;
+        $k++;
+    }
+    while ($j <= $right) {
+        $sorted_nums[$k] = $index_map[$j];
+        $j++;
+        $k++;
+    }
+    for ($k = $left; $k <= $right; $k++) {
+        $index_map[$k] = $sorted_nums[$k];
+    }
 }
 }
