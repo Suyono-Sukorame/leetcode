2090b372ed02093495ccef21a4119d400eda7ece
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-17 07:25:03 +0800

    521. Longest Uncommon Subsequence I

---
+++ php/521. Longest Uncommon Subsequence I.php
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param String $a
+     * @param String $b
+     * @return Integer
+     */
+    function findLUSlength($a, $b) {
+        if ($a == $b) {
+            return -1;
+        }
+        return max(strlen($a), strlen($b));
+    }
+}
