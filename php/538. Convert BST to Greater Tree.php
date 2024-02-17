b1801993f6943f6fee6db462a9b5f266b1c19239
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-17 17:44:22 +0800

    538. Convert BST to Greater Tree

---
+++ php/538. Convert BST to Greater Tree.php
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private $sum = 0;
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function convertBST($root) {
+        if ($root === null) {
+            return null;
+        }
+
+        $this->convertBST($root->right);
+
+        $this->sum += $root->val;
+        $root->val = $this->sum;
+
+        $this->convertBST($root->left);
+
+        return $root;
+    }
+}
