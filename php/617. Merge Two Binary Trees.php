ad29788a5d3168faf727184bdd54203d231efaa7
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-19 18:07:53 +0800

    617. Merge Two Binary Trees

---
+++ php/617. Merge Two Binary Trees.php
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root1
+     * @param TreeNode $root2
+     * @return TreeNode
+     */
+    function mergeTrees($root1, $root2) {
+        if ($root1 === null) return $root2;
+        if ($root2 === null) return $root1;
+        
+        $mergedVal = $root1->val + $root2->val;
+        
+        $mergedRoot = new TreeNode($mergedVal);
+        
+        $mergedRoot->left = $this->mergeTrees($root1->left, $root2->left);
+        $mergedRoot->right = $this->mergeTrees($root1->right, $root2->right);
+        
+        return $mergedRoot;
+    }
+}
