7f5dafeea62865933312cbcdf51c853c3bdf4a61
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-02-25 14:36:31 +0800

    717. 1-bit and 2-bit Characters

---
+++ php/717. 1-bit and 2-bit Characters.php
@@ -0,0 +1,21 @@
+class Solution {
+
+/**
+ * @param Integer[] $bits
+ * @return Boolean
+ */
+function isOneBitCharacter($bits) {
+    $i = 0;
+    $n = count($bits);
+    
+    while ($i < $n - 1) {
+        if ($bits[$i] == 1) {
+            $i += 2;
+        } else {
+            $i++;
+        }
+    }
+    
+    return $i == $n - 1;
+}
+}
