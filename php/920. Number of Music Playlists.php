2f133d396d2ee0035e45785c0401fb91aa7fc4b9
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-03-04 18:20:03 +0800

    920. Number of Music Playlists

---
+++ php/920. Number of Music Playlists.php
@@ -0,0 +1,25 @@
+class Solution {
+
+/**
+ * @param Integer $n
+ * @param Integer $goal
+ * @param Integer $k
+ * @return Integer
+ */
+function numMusicPlaylists($n, $goal, $k) {
+    $mod = 1000000007;
+    $dp = array_fill(0, $goal + 1, array_fill(0, $n + 1, 0));
+    $dp[0][0] = 1;
+
+    for ($i = 1; $i <= $goal; $i++) {
+        for ($j = 1; $j <= $n; $j++) {
+            $dp[$i][$j] = ($dp[$i - 1][$j - 1] * ($n - $j + 1)) % $mod;
+            if ($j > $k) {
+                $dp[$i][$j] = ($dp[$i][$j] + ($dp[$i - 1][$j] * ($j - $k)) % $mod) % $mod;
+            }
+        }
+    }
+
+    return $dp[$goal][$n];
+}
+}
