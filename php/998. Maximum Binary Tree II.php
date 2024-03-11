0b26c7be56958a374ddb8a4bf83559bd7d755386
Suyono-Sukorame <suyonosukorame@ymail.com>
2024-03-11 10:46:51 +0800

    998. Maximum Binary Tree II

---
+++ php/998. Maximum Binary Tree II.php
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @param Integer $val
+     * @return TreeNode
+     */
+    function insertIntoMaxTree($root, $val) {
+        if ($root == null || $root->val < $val) {
+            $newNode = new TreeNode($val);
+            $newNode->left = $root;
+            return $newNode;
+        }
+        
+        $root->right = $this->insertIntoMaxTree($root->right, $val);
+        return $root;
+    }
+}
